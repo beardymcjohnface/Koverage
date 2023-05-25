aa9fa5b44b11b1e38c298e9af07dd6bf5afdaf58
Michael Roach <beardymcjohnface@gmail.com>
2023-05-26 09:26:09 +0930

    DEV: add some missing packages

--- setup.py
+++ setup.py
@@ -55,7 +55,9 @@ setup(
         "snakemake>=7.14.0",
         "pyyaml>=6.0",
         "Click>=8.1.3",
-        "attrmap>=0.0.7"
+        "attrmap>=0.0.7",
+        "zstandard>=0.21.0",
+        "numpy>=1.24.3",
     ],
     entry_points={
         "console_scripts": [
