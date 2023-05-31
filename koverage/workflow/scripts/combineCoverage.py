38338e9ecf02fd4e5e70c8abc7460dc3297bf9f0
Michael Roach <beardymcjohnface@gmail.com>
2023-05-31 15:06:09 +0930

    Dev: bugfixes

--- koverage/workflow/scripts/combineCoverage.py
+++ koverage/workflow/scripts/combineCoverage.py
@@ -16,20 +16,24 @@ with open(snakemake.input[0], 'r') as infh:
         try:
             assert(type(allCoverage[l[1]]) is dict)
         except (AssertionError, KeyError) as err:
-            allCoverage[l[1]] = {'rpm':0,'rpkm':0,'rpk':0,'tpm':0}
-        allCoverage[l[1]]['rpm'] += int(l[3])
-        allCoverage[l[1]]['rpkm'] += int(l[4])
-        allCoverage[l[1]]['rpk'] += int(l[5])
-        allCoverage[l[1]]['tpm'] += int(l[6])
+            allCoverage[l[1]] = {'count':0,'rpm':0,'rpkm':0,'rpk':0,'tpm':0}
+        allCoverage[l[1]]['count'] += int(l[2])
+        allCoverage[l[1]]['rpm'] += float(l[3])
+        allCoverage[l[1]]['rpkm'] += float(l[4])
+        allCoverage[l[1]]['rpk'] += float(l[5])
+        allCoverage[l[1]]['tpm'] += float(l[6])
 
 logging.debug("Printing all sample coverage")
 
 with open(snakemake.output.all_cov, 'w') as outCov:
+    outCov.write("Contig\tCount\tRPM\tRPKM\tRPK\tTPM\n")
     for contig in sorted(allCoverage.keys()):
         outCov.write("\t".join([
-            str(allCoverage[contig]['rpm']),
-            str(allCoverage[contig]['rpkm']),
-            str(allCoverage[contig]['rpk']),
-            str(allCoverage[contig]['tpm']) + "\n"
+            contig,
+            str(allCoverage[contig]['count']),
+            "{:.{}g}".format(allCoverage[contig]["rpm"], 4),
+            "{:.{}g}".format(allCoverage[contig]["rpkm"], 4),
+            "{:.{}g}".format(allCoverage[contig]["rpk"], 4),
+            "{:.{}g}".format(allCoverage[contig]["tpm"], 4) + "\n"
         ]))
 
