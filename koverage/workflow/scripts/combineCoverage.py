884652250da972554fd64ea05150916a9a665c6f
Michael Roach <beardymcjohnface@gmail.com>
2023-06-01 17:04:20 +0930

    Dev: unit tests for combineCoverage.py, more to come

--- koverage/workflow/scripts/combineCoverage.py
+++ koverage/workflow/scripts/combineCoverage.py
@@ -3,37 +3,56 @@
 import logging
 
 
-logging.basicConfig(filename=snakemake.log[0], filemode="w", level=logging.DEBUG)
+def collect_coverage_stats(input_file):
+    """Combine the mapped coverage stats for all samples.
+
+    :param input_file: Text TSV file (Sample\tContig\tCount\tRPM\tRPKM\tRPK\tTPM\tHitrate\tVariance)
+    :returns all_coverage: Dictionary of counts for each contig (dict[contigID]["count"/"rpm"/"rpkm"/"rpk"/"tpm"])
+    """
+    all_coverage = {}
+    with open(input_file, "r") as infh:
+        infh.readline()
+        for line in infh:
+            l = line.strip().split()
+            try:
+                assert(type(all_coverage[l[1]]) is dict)
+            except (AssertionError, KeyError):
+                all_coverage[l[1]] = {"count": 0, "rpm": 0, "rpkm": 0, "rpk": 0, "tpm": 0}
+            all_coverage[l[1]]["count"] += int(l[2])
+            all_coverage[l[1]]["rpm"] += float(l[3])
+            all_coverage[l[1]]["rpkm"] += float(l[4])
+            all_coverage[l[1]]["rpk"] += float(l[5])
+            all_coverage[l[1]]["tpm"] += float(l[6])
+    return all_coverage
 
 
-logging.debug("Collecting combined coverage stats")
+def print_sample_coverage(output_file, all_coverage):
+    """Print the combined coverage statistics from collect_coverage_stats().
 
-allCoverage = dict()
-with open(snakemake.input[0], "r") as infh:
-    infh.readline()
-    for line in infh:
-        l = line.strip().split()
-        try:
-            assert(type(allCoverage[l[1]]) is dict)
-        except (AssertionError, KeyError) as err:
-            allCoverage[l[1]] = {"count":0,"rpm":0,"rpkm":0,"rpk":0,"tpm":0}
-        allCoverage[l[1]]["count"] += int(l[2])
-        allCoverage[l[1]]["rpm"] += float(l[3])
-        allCoverage[l[1]]["rpkm"] += float(l[4])
-        allCoverage[l[1]]["rpk"] += float(l[5])
-        allCoverage[l[1]]["tpm"] += float(l[6])
+    :param output_file: Text TSV filepath for writing
+    :param all_coverage: Dictionary of counts for each contig (dict[contigID]["count"/"rpm"/"rpkm"/"rpk"/"tpm"])
+    :returns: None
+    """
+    with open(output_file, "w") as outCov:
+        outCov.write("Contig\tCount\tRPM\tRPKM\tRPK\tTPM\n")
+        for contig in sorted(all_coverage.keys()):
+            outCov.write("\t".join([
+                contig,
+                str(all_coverage[contig]["count"]),
+                "{:.{}g}".format(all_coverage[contig]["rpm"], 4),
+                "{:.{}g}".format(all_coverage[contig]["rpkm"], 4),
+                "{:.{}g}".format(all_coverage[contig]["rpk"], 4),
+                "{:.{}g}".format(all_coverage[contig]["tpm"], 4) + "\n"
+            ]))
 
-logging.debug("Printing all sample coverage")
 
-with open(snakemake.output.all_cov, "w") as outCov:
-    outCov.write("Contig\tCount\tRPM\tRPKM\tRPK\tTPM\n")
-    for contig in sorted(allCoverage.keys()):
-        outCov.write("\t".join([
-            contig,
-            str(allCoverage[contig]["count"]),
-            "{:.{}g}".format(allCoverage[contig]["rpm"], 4),
-            "{:.{}g}".format(allCoverage[contig]["rpkm"], 4),
-            "{:.{}g}".format(allCoverage[contig]["rpk"], 4),
-            "{:.{}g}".format(allCoverage[contig]["tpm"], 4) + "\n"
-        ]))
+def main(input_file, output_file, log_file):
+    logging.basicConfig(filename=log_file, filemode="w", level=logging.DEBUG)
+    logging.debug("Collecting combined coverage stats")
+    all_coverage = collect_coverage_stats(input_file)
+    logging.debug("Printing all sample coverage")
+    print_sample_coverage(output_file, all_coverage)
 
+
+if __name__ == "__main__":
+    main(snakemake.input[0], snakemake.output.all_cov, snakemake.log[0])
