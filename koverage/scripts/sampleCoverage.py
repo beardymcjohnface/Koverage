614cc36f38dc455e4a9975f5973fb6c02404f620
Michael Roach <beardymcjohnface@gmail.com>
2023-06-07 16:18:13 +0930

    Dev: refactoring for unit tests work in progress

--- koverage/scripts/sampleCoverage.py
+++ koverage/scripts/sampleCoverage.py
@@ -3,48 +3,35 @@
 import logging
 
 
-logging.basicConfig(filename=snakemake.log[0], filemode="w", level=logging.DEBUG)
+def slurp_variance(variance_file):
+    variance = dict()
+    hitrate = dict()
+    with open(variance_file, 'r') as varfh:
+        for line in varfh:
+            l = line.strip().split()
+            hitrate[l[0]] = l[1]
+            variance[l[0]] = l[2]
+    return variance, hitrate
 
 
-logging.debug("Slurping variance")
-
-
-var = dict()
-hitrate = dict()
-
-with open(snakemake.input.var, 'r') as varfh:
-    for line in varfh:
-        l = line.strip().split()
-        hitrate[l[0]] = l[1]
-        var[l[0]] = l[2]
-
-
-logging.debug("Reading in library size")
-
-
-with open(snakemake.input.lib, 'r') as f:
-    rpmscale = int(f.readline().strip()) / 1000000                    # Count up the total reads in a sample and divide that number by 1,000,000 – this is our “per million” scaling factor.
-
-
-logging.debug("Parsing contig counts and calculating rpm rpkm rpk")
-
-
-allRpk = list()
-
-with open(snakemake.input.counts, 'r') as t:
+def calculate_coverage_stats_from_counts(lib_file):
+    with open(lib_file, 'r') as f:
+        rpmscale = int(f.readline().strip()) / 1000000                    # Count up the total reads in a sample and divide that number by 1,000,000 – this is our “per million” scaling factors
+    allRpk = list()
     counts = dict()
-    for line in t:
-        l = line.strip().split()
-        lenkb = int(l[1]) / 1000
-        counts[l[0]] = dict()
-        counts[l[0]]["count"] = l[2]
-        counts[l[0]]["rpm"] = int(l[2]) / rpmscale              # Divide the read counts by the “per million” scaling factor. This normalizes for sequencing depth, giving you reads per million (RPM)
-        counts[l[0]]["rpkm"] = counts[l[0]]["rpm"] / lenkb      # Divide the RPM values by the length of the gene, in kilobases. This gives you RPKM.
-        rpk = int(l[2]) / lenkb                                 # Divide the read counts by the length of each gene in kilobases. This gives you reads per kilobase (RPK).
-        counts[l[0]]["rpk"] = rpk
-        allRpk.append(rpk)
-rpkscale = sum(allRpk) / 1000000                                # Count up all the RPK values in a sample and divide this number by 1,000,000. This is your “per million” scaling factor.
-
+    with open(snakemake.input.counts, 'r') as t:
+        for line in t:
+            l = line.strip().split()
+            lenkb = int(l[1]) / 1000
+            counts[l[0]] = dict()
+            counts[l[0]]["count"] = l[2]
+            counts[l[0]]["rpm"] = int(l[2]) / rpmscale              # Divide the read counts by the “per million” scaling factor. This normalizes for sequencing depth, giving you reads per million (RPM)
+            counts[l[0]]["rpkm"] = counts[l[0]]["rpm"] / lenkb      # Divide the RPM values by the length of the gene, in kilobases. This gives you RPKM.
+            rpk = int(l[2]) / lenkb                                 # Divide the read counts by the length of each gene in kilobases. This gives you reads per kilobase (RPK).
+            counts[l[0]]["rpk"] = rpk
+            allRpk.append(rpk)
+    rpkscale = sum(allRpk) / 1000000                                # Count up all the RPK values in a sample and divide this number by 1,000,000. This is your “per million” scaling factor.
+    return counts, rpkscale
 
 logging.debug("Calculating TPMs and printing")
 
@@ -67,3 +54,18 @@ with open(snakemake.output[0], 'w') as o:
             hitrate[contig],
             var[contig] + "\n"
         ]))
+
+
+def main(**kwargs):
+    logging.basicConfig(filename=kwargs["log_file"], filemode="w", level=logging.DEBUG)
+    logging.debug("Slurping variance")
+    variance, hitrate = slurp_variance(kwargs["variance_file"])
+    logging.debug("Reading in library size")
+
+
+
+if __name__ == "__main__":
+    main(variance_file=snakemake.input.var,
+         lib_file=snakemake.input.lib,
+         log_file=snakemake.log[0],
+         )
