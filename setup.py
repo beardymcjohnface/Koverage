f5fcdaa0fb7fef554905ff44d47e1b3beb23ad1f
Michael Roach <beardymcjohnface@gmail.com>
2023-08-15 16:38:45 +0930

    DEV: Check if newest snakemake versions work

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setup(
     install_requires=[
         "snaketool-utils>=0.0.3",
         "metasnek>=0.0.6",
-        "snakemake>=7.14.0,<=7.26.0",
+        "snakemake>=7.14.0",
         "pyyaml>=6.0",
         "Click>=8.1.3",
         "zstandard>=0.21.0",
